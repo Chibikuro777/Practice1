8d6a76639951b3d52c156fd44eb11ba6d73c87a8
川阪佐奈恵 <sanae.kawasaka@gmail.com>
2020-01-28 23:12:05 +0900

    未完成

---
+++ check.php
@@ -0,0 +1,43 @@
+<?php
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $submit = $_POST['submit'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>確認フォーム</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<form action="thanks.php" method="post">
+    <h1>確認フォーム</h1>
+    <?php if ($nickname =='' || $email ==''): ?>
+    <p>入力されていません</p>
+    <input type="submit" name="return" value="戻る" class="submit">
+
+    <div class="content">
+    <input type="hidden" name="nickname" value=""class="">
+    </div>
+    <div class="content">
+    <input type="hidden" name="email" value="">
+    </div>
+    
+    <div class="content-button">
+    <input type="hidden" name="return" value="戻る" class="submit">
+    <input type="hidden" name="submit" value="確認" class="submit">
+    <?php elseif ($nickname != '' && $email != ''): ?>
+    <p><?php echo "あなたのニックネームは{$nickname}です"?></p>
+    <p><?php echo "あなたのメールアドレスは{$email}です"?></p>
+    <input type="submit" name="return" value="戻る" class="submit">
+    <input type="submit" name="submit" value="確認" class="submit">
+    <?php endif; ?>
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practice2</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <form action="check.php" method="post">
+    <h1>入力フォーム</h1>
+    <div class="content">
+    ニックネーム<input type="text" name="nickname" value=""class="">
+    </div>
+    <div class="content">
+    メールアドレス<input type="text" name="email" value="">
+    </div>
+    <input type="submit" name="submit" value="確認" class="submit">
+    </form>
+</body>
+</html>
